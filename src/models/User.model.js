bc87c25fb648fcc02f9b3f1408d6fb2f0a972831
Raiyan <tajkierhaque@gmail.com>
2022-12-05 13:46:45 +0600

    code formatting done

--- src/models/User.model.js
+++ src/models/User.model.js
@@ -1,39 +1,38 @@
-const mongoose = require("mongoose");
-const bcrypt = require("bcryptjs");
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
 
 const userSchema = mongoose.Schema(
-  {
-    name: {
-      type: String,
-      required: true,
-      trim: true,
-      maxLength: 6,
+    {
+        name: {
+            type: String,
+            required: true,
+            trim: true,
+            maxLength: 6,
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            lowercase: true,
+        },
+        password: {
+            type: String,
+            required: true,
+        },
+        profile: {
+            type: String,
+            default: 'https://i.pinimg.com/originals/7d/34/d9/7d34d9d53640af5cfd2614c57dfa7f13.png',
+        },
     },
-    email: {
-      type: String,
-      required: true,
-      unique: true,
-      lowercase: true,
-    },
-    password: {
-      type: String,
-      required: true,
-    },
-    profile: {
-      type: String,
-      default:
-        "https://i.pinimg.com/originals/7d/34/d9/7d34d9d53640af5cfd2614c57dfa7f13.png",
-    },
-  },
-  { timestamps: true }
+    { timestamps: true }
 );
 
-userSchema.pre("save", async function (next) {
-  if (!this.isModified("password")) {
+userSchema.pre('save', async function (next) {
+    if (!this.isModified('password')) {
+        next();
+    }
+    this.password = await bcrypt.hash(this.password, 10);
     next();
-  }
-  this.password = await bcrypt.hash(this.password, 10);
-  next();
 });
 
-module.exports = mongoose.model("User", userSchema);
+module.exports = mongoose.model('User', userSchema);
--- src/routes/auth/auth.controller.js
+++ src/routes/auth/auth.controller.js
@@ -1,70 +1,69 @@
-const { generateAccessToken, generateRefreshToken } = require("../../lib/jwt");
-const createAsyncError = require("../../middlewares/createAsyncError");
-const User = require("../../models/User.model");
-const jwt = require("jsonwebtoken");
+const jwt = require('jsonwebtoken');
+const { generateAccessToken, generateRefreshToken } = require('../../lib/jwt');
+const createAsyncError = require('../../middlewares/createAsyncError');
+const User = require('../../models/User.model');
 
 // @desc Register
 // @route POST api/auth/register
 // @access Register a user
 const register = createAsyncError(async (req, res, _next) => {
-  const { name, email, password, profile } = req.body;
+    const { name, email, password, profile } = req.body;
 
-  if (!name || !email || !password || !profile) {
-    res.status(400).json({ message: "All the fields are required" });
-  }
+    if (!name || !email || !password || !profile) {
+        res.status(400).json({ message: 'All the fields are required' });
+    }
 
-  const isEmailExitsOrNot = await User.findOne({ email });
+    const isEmailExitsOrNot = await User.findOne({ email });
 
-  if (isEmailExitsOrNot) {
-    res.status(500).json({ message: "User already exists" });
-  }
+    if (isEmailExitsOrNot) {
+        res.status(500).json({ message: 'User already exists' });
+    }
 
-  const newUser = new User(req.body);
+    const newUser = new User(req.body);
 
-  await newUser.save();
+    await newUser.save();
 
-  res.status(200).json({ message: "Registration successful go to Login" });
+    res.status(200).json({ message: 'Registration successful go to Login' });
 });
 
 // @desc Login
 // @route POST api/auth/login
 // @access For log in a user
 const login = createAsyncError(async (req, res, _next) => {
-  const { email, password } = req.body;
-  if (!email || !password) {
-    res.status(400).json({ message: "All the fields are required" });
-  }
-  const isEmailExitsOrNot = await User.findOne({ email });
+    const { email, password } = req.body;
+    if (!email || !password) {
+        res.status(400).json({ message: 'All the fields are required' });
+    }
+    const isEmailExitsOrNot = await User.findOne({ email });
 
-  if (!isEmailExitsOrNot) {
-    res.status(500).json({ message: "User not exists" });
-  }
+    if (!isEmailExitsOrNot) {
+        res.status(500).json({ message: 'User not exists' });
+    }
 
-  const createdAccessToken = generateAccessToken(isEmailExitsOrNot._id);
-  const createdRefreshToken = generateRefreshToken(isEmailExitsOrNot._id);
+    const createdAccessToken = generateAccessToken(isEmailExitsOrNot._id);
+    const createdRefreshToken = generateRefreshToken(isEmailExitsOrNot._id);
 
-  res.cookie("token", createdRefreshToken, {
-    httpOnly: true,
-    path: "/",
-    maxAge: 7 * 24 * 60 * 60 * 1000, // for 7 days
-    secure: true,
-    sameSite: "None",
-  });
+    res.cookie('token', createdRefreshToken, {
+        httpOnly: true,
+        path: '/',
+        maxAge: 7 * 24 * 60 * 60 * 1000, // for 7 days
+        secure: true,
+        sameSite: 'None',
+    });
 
-  res
-    .status(200)
-    .json({ message: "Logged in Successfully", token: createdAccessToken });
+    res.status(200).json({ message: 'Logged in Successfully', token: createdAccessToken });
 });
 
 // @desc Refresh
 // @route Get api/auth/refresh
 // @access to create new access token after expires
 
-const refresh = createAsyncError((req, res) => {
-  const cookies = req.cookies;
-  if (!cookies?.token) {
-    res.status(401).json({ message: "UnAuthorized" });
-  }
+const refresh = createAsyncError(async (req, res) => {
+    const { cookies } = req;
+    if (!cookies?.token) {
+        res.status(401).json({ message: 'UnAuthorized' });
+    }
+    const verifyJwt = await jwt.verify(cookies.token, process.env.REFRESH_TOKEN_SECRET);
 });
 
 module.exports = { register, login };
